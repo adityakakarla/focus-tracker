639dcb21e59a8238c45c898a98394a91862230f1
adityakakarla <85147239+adityakakarla@users.noreply.github.com>
2023-09-06 16:09:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ import os
 st.title("YOLOv5 Object Detection")
 
 if st.button("Detect Objects"):
-    os.system('python phone_detector.py --weights phone_detector.pt --source 1')
+    os.system('python phone_detector.py --weights phone_detector.pt --source 0')
